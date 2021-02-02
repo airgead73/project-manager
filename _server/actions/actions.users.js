0794587fc9cbca9d54f7b562208a5c5774851e03
Dan Moneypenny <catullus1973@gmail.com>
2021-02-02 15:54:50 -0800

    add user actions

---
+++ _server/actions/actions.users.js
@@ -0,0 +1,63 @@
+const asyncHandler = require('../middleware/handleAsync');
+//const User = require('');
+const createError = require('http-errors');
+
+/**
+ * @route   POST /
+ * @desc    create 
+ * @access  private
+ */
+
+exports.create = asyncHandler(async function(req, res, next) {
+  res.send('POST: create');
+});
+
+/**
+ * @route   GET /
+ * @desc    read
+ * @access  private
+ */
+
+exports.read = asyncHandler(async function(req, res, next) {
+  res.send('GET: read');
+});
+
+/**
+ * @route   GET /:ID
+ * @desc    read one
+ * @access  private
+ */ 
+
+exports.read_one = asyncHandler(async function(req, res, next) {
+  res.send('GET: read one');
+});
+
+/**
+ * @route   PUT /:ID
+ * @desc    update user
+ * @access  private
+ */ 
+
+exports.update = asyncHandler(async function(req, res, next) {
+  res.send('PUT: update');
+}); 
+
+/**
+ * @route   DELETE /:ID
+ * @desc    delete user
+ * @access  private
+ */ 
+
+exports.delete = asyncHandler(async function(req, res, next) {
+  res.send('DELETE: delete');
+});
+
+/**
+ * @route   DELETE /
+ * @desc    delete all
+ * @access  private
+ */
+
+exports.delete_all = asyncHandler(async function(req, res, next) {
+  res.send('DELETE: all');
+});
